1fedaeca19aa2d658b3543e5679d4d10a1433b44
Adam Eberle <aceberle@gmail.com>
2021-08-31 21:11:20 -0400

    Enhancing cell value extraction and row/cell processing
    Using DataFormatter to extract better cell representation when numbers
    are involved.  Also, stopping row/cell processing when blank cells are
    encountered.

--- src/main/java/com/eberlecreative/pspiindexgenerator/datafileparser/DataFileParser.java
+++ src/main/java/com/eberlecreative/pspiindexgenerator/datafileparser/DataFileParser.java
@@ -1,5 +1,6 @@
 package com.eberlecreative.pspiindexgenerator.datafileparser;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,15 +8,17 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.CaseUtils;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DataFileParser {
 
-    public List<Map<String, String>> parseDataFile(String dataFilePath) {
+    public List<Map<String, String>> parseDataFile(File dataFilePath) {
         final List<Map<String, String>> results = new ArrayList<>();
         try (FileInputStream fis = new FileInputStream(dataFilePath);
                 XSSFWorkbook workbook = new XSSFWorkbook (fis);) {
@@ -31,19 +34,26 @@ public class DataFileParser {
                 final Cell cell = headerCellIterator.next();
                 final String cellValue = cell.getStringCellValue();
                 final String header = canonicalizeHeader(cellValue);
+                if(StringUtils.isBlank(header)) {
+                    break;
+                }
                 headers.add(header);
             }
+            final DataFormatter objDefaultFormat = new DataFormatter();
             while(rowIterator.hasNext()) {
                 final Map<String, String> rowData = new HashMap<>();
-                results.add(rowData);
                 final Row row = rowIterator.next();
                 final Iterator<Cell> cellIterator = row.cellIterator();
                 for(int i = 0; i < headers.size() && cellIterator.hasNext(); i++) {
-                    final Cell cell = cellIterator.next();
-                    final String cellValue = cell.getStringCellValue();
                     final String header = headers.get(i);
+                    final Cell cell = cellIterator.next();
+                    final String cellValue = objDefaultFormat.formatCellValue(cell);
                     rowData.put(header, cellValue);
                 }
+                if(isAllBlank(rowData)) {
+                    break;
+                }
+                results.add(rowData);
             }
         } catch (RuntimeException e) {
             throw e;
@@ -51,6 +61,10 @@ public class DataFileParser {
             throw new RuntimeException("An exception occurred while processing data file at: " + dataFilePath, e);
         }
         return results;
+    }
+
+    private boolean isAllBlank(Map<String, String> rowData) {
+        return StringUtils.isAllBlank(rowData.values().toArray(new String[0]));
     }
 
     private String canonicalizeHeader(String value) {
